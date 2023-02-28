64ec81e96dd31340ef0e09ac2ad7dc77b2834f62
Mahesh12345678910 <maheshvangala1997@gmail.com>
2023-02-28 12:45:43 +0530

    commit

--- search/search.js
+++ search/search.js
@@ -2,97 +2,115 @@ let GC_Prods=[
     {
         prod_name : "Corduroy Blazer",
         src:"https://ats-sb.adobe.com/jjsite/include/images/man-active3.jpg",
-        path:"../men/activewear/corduroy_blazer.html"
+        path:"../men/activewear/corduroy_blazer.html",
+        prod_id : "200240"
 
     },
     {
         prod_name : "Hooded Sweater",
         src:"https://ats-sb.adobe.com/jjsite/include/images/man-active2.jpg",
-        path:"../men/activewear/hooded_sweater.html"
+        path:"../men/activewear/hooded_sweater.html",
+        prod_id : "200233"
 
     },
     {
         prod_name : "Military Jacket",
         src:"https://ats-sb.adobe.com/jjsite/include/images/active_men.jpg",
-        path:"../men/activewear/military_jacket.html"
+        path:"../men/activewear/military_jacket.html",
+        prod_id : "200241"
 
     },
     {
         prod_name : "Sports Jacket",
         src:"https://ats-sb.adobe.com/jjsite/include/images/man-active4.jpg",
-        path:"../men/activewear/sport_jacket.html"
+        path:"../men/activewear/sport_jacket.html",
+        prod_id : "200244"
 
     },
     {
         prod_name : "Blue Shirt",
         src:"https://ats-sb.adobe.com/jjsite/include/images/man-apparel4.jpg",
-        path:"../men/fineapparel/blueshirt.html"
+        path:"../men/fineapparel/blueshirt.html",
+        prod_id : "201241"
 
     },
     {
         prod_name : "French Cuff Shirt",
         src:"https://ats-sb.adobe.com/jjsite/include/images/man-apparel3.jpg",
-        path:"../men/fineapparel/frenchcuffshirt.html"
+        path:"../men/fineapparel/frenchcuffshirt.html",
+        prod_id : "201244"
 
     },
     {
         prod_name : "Tuxedo",
         src:"https://ats-sb.adobe.com/jjsite/include/images/man-apparel2.jpg",
-        path:"../men/fineapparel/tuxedo.html"
+        path:"../men/fineapparel/tuxedo.html",
+        prod_id : "201233"
 
     },
     {
         prod_name : "Two-button Suit",
         src:"https://ats-sb.adobe.com/jjsite/include/images/fa_man.jpg",
-        path:"../men/fineapparel/twobuttonsuit.html"
+        path:"../men/fineapparel/twobuttonsuit.html",
+        prod_id : "201240"
 
     },
     {
         prod_name : "Front-Zip Ski Jacket",
         src:"https://ats-sb.adobe.com/jjsite/include/images/active3.jpg",
-        path:"../women/activewear/frontzip_ski_jacket.html"
+        path:"../women/activewear/frontzip_ski_jacket.html",
+        prod_id : "100244"
 
     },
     {
         prod_name : "Fur Trim Ski Jacket",
         src:"https://ats-sb.adobe.com/jjsite/include/images/active2.jpg",
-        path:"../women/activewear/fur_trim_ski_jacket.html"
+        path:"../women/activewear/fur_trim_ski_jacket.html",
+        prod_id : "100241"
 
     },
     {
         prod_name : "Scrunched Leg Cargo Pants",
         src:"https://ats-sb.adobe.com/jjsite/include/images/active4.jpg",
-        path:"../women/activewear/scrunched_leg_cargo_pants.html"
+        path:"../women/activewear/scrunched_leg_cargo_pants.html",
+        prod_id : "100233"
 
     },
     {
         prod_name : "Tie-Waist Puffer Jacket",
         src:"https://ats-sb.adobe.com/jjsite/include/images/active1.jpg",
-        path:"../women/activewear/waist_puffer_jacket.html"
+        path:"../women/activewear/waist_puffer_jacket.html",
+        prod_id : "100240"
 
     },
     {
         prod_name : "Black Dress",
         src:"https://ats-sb.adobe.com/jjsite/include/images/dress.jpg",
-        path:"../women/fineapparel/black_dress.html"
+        path:"../women/fineapparel/black_dress.html",
+        prod_id : "101340"
 
     },
     {
         prod_name : "Red Dress",
         src:"https://ats-sb.adobe.com/jjsite/include/images/reddress.jpg",
-        path:"../women/fineapparel/red_dress.html"
+        path:"../women/fineapparel/red_dress.html",
+        prod_id : "101341"
 
     },
     {
         prod_name : "Sequine Dress",
         src:"https://ats-sb.adobe.com/jjsite/include/images/sequin_dress.jpg",
-        path:"../women/fineapparel/sequin_dress.html"
+        path:"../women/fineapparel/sequin_dress.html",
+            prod_id: "101333",
+            
+
 
     },
     {
         prod_name : "Silk Dress",
         src:"https://ats-sb.adobe.com/jjsite/include/images/silk_dress.jpg",
-        path:"../women/fineapparel/silk_dress.html"
+        path:"../women/fineapparel/silk_dress.html",
+        prod_id : "101344"
 
     }
 ]
