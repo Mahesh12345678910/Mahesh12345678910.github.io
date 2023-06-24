674ff14d90f73123f9e9320e778c30ffde7dbd72
Mahesh Vangala <maheshvangala1997@gmail.com>
2023-06-24 19:47:50 +0530

    commit

--- dataLayer.js
+++ dataLayer.js
@@ -29,7 +29,7 @@
                     
                     userInfo:{
 
-                    userEmail: "",
+
                     userID : "",
                     userStatus : ""
                     }
