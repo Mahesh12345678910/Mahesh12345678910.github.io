9a7853dcb2de8d08a400c51a359c5a24f86566fa
Mahesh Vangala <maheshvangala1997@gmail.com>
2023-07-02 11:11:05 +0530

    commit

--- js/launch_envChange.js
+++ js/launch_envChange.js
@@ -1,3 +1,6 @@
-let launch_script=document.getElementById("launch_script");
-launch_script.src="https://assets.adobedtm.com/4f2e5e0df740/9d18784bf836/launch-46ff047ac3db-development.min.js"
+function changeEnv(){
+    return "https://assets.adobedtm.com/4f2e5e0df740/9d18784bf836/launch-46ff047ac3db-development.min.js"
+}
+
+
 
