209fc0ecf917387502a46413c1922661da5b74a2
Mahesh12345678910 <maheshvangala1997@gmail.com>
2023-07-28 18:19:56 +0530

    Commiy

--- add_launch_script.js
+++ add_launch_script.js
@@ -5,6 +5,7 @@ function add_launch_script()
         let head=document.getElementsByTagName("head")[0];
 
         let launch_script=document.createElement("script");
+        // https://assets.adobedtm.com/01296dd00565/885ffeb5e57f/launch-e1386bb73e46-development.min.js
         launch_script.src="https://assets.adobedtm.com/4f2e5e0df740/9d18784bf836/launch-46ff047ac3db-development.min.js";
         launch_script.async=true;
 
@@ -15,3 +16,18 @@ function add_launch_script()
     }
 
     add_launch_script();
+
+
+/*AEP GDC DEV ENABLEMENT 2 SANDBOX> Analytics Team Enablement(VA7)>AEP MVANGALA
+  https://assets.adobedtm.com/01296dd00565/885ffeb5e57f/launch-e1386bb73e46-development.min.js
+
+
+AGS1871>MV_PRACTICE_APPMEASUREMENT
+  https://assets.adobedtm.com/4f2e5e0df740/9d18784bf836/launch-46ff047ac3db-development.min.js 
+  
+  
+  AGS1871>Mahesh Enablement Web Property
+
+    https://assets.adobedtm.com/4f2e5e0df740/7cca5b6f22d0/launch-a539f945e683-development.min.js
+    
+  */
