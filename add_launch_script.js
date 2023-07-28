9e9ade40b12d52d84bf242ee0b15d598e1281ef1
Mahesh12345678910 <maheshvangala1997@gmail.com>
2023-07-28 18:23:19 +0530

    commit2

--- add_launch_script.js
+++ add_launch_script.js
@@ -6,7 +6,7 @@ function add_launch_script()
 
         let launch_script=document.createElement("script");
         // https://assets.adobedtm.com/01296dd00565/885ffeb5e57f/launch-e1386bb73e46-development.min.js
-        launch_script.src="https://assets.adobedtm.com/4f2e5e0df740/9d18784bf836/launch-46ff047ac3db-development.min.js";
+        launch_script.src="https://assets.adobedtm.com/01296dd00565/885ffeb5e57f/launch-e1386bb73e46-development.min.js";
         launch_script.async=true;
 
         dataLayer_script.parentElement.insertBefore(launch_script,dataLayer_script.nextElementSibling);
@@ -29,5 +29,5 @@ AGS1871>MV_PRACTICE_APPMEASUREMENT
   AGS1871>Mahesh Enablement Web Property
 
     https://assets.adobedtm.com/4f2e5e0df740/7cca5b6f22d0/launch-a539f945e683-development.min.js
-    
+
   */
