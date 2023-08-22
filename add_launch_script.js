c8558d708e0e8c68db867f1e02683f0727069965
Mahesh12345678910 <maheshvangala1997@gmail.com>
2023-08-22 15:29:29 +0530

    AEP VANGALA-GDC DEV ENABLEMENT

--- add_launch_script.js
+++ add_launch_script.js
@@ -1,4 +1,11 @@
 
+let envs={
+ "AEP VANGALA-GDC DEV ENABLEMENT" : "https://assets.adobedtm.com/01296dd00565/885ffeb5e57f/launch-e1386bb73e46-development.min.js",
+ "MV_PRACTICE_APPMEASUREMENT-SELF" : "https://assets.adobedtm.com/4f2e5e0df740/9d18784bf836/launch-46ff047ac3db-development.min.js",
+ "Mahesh Enablement Web Property-SELF" : "https://assets.adobedtm.com/4f2e5e0df740/7cca5b6f22d0/launch-a539f945e683-development.min.js"
+
+}
+
 
 let dataLayer_script=document.createElement("script");
 dataLayer_script.id="dataLayer_script";
@@ -6,12 +13,15 @@ dataLayer_script.innerHTML="window.adobeDataLayer = window.adobeDataLayer || [];
 let headTag=document.getElementsByTagName("head")[0];
 
 let launch_script_tag=document.createElement("script");
-launch_script_tag.src="https://assets.adobedtm.com/4f2e5e0df740/9d18784bf836/launch-46ff047ac3db-development.min.js"
+launch_script_tag.src= envs["AEP VANGALA-GDC DEV ENABLEMENT"];
 launch_script_tag.async=true;
 
 
 headTag.append(dataLayer_script)
 headTag.append(launch_script_tag)
+
+
+
 
 // let launch_script="https://assets.adobedtm.com/4f2e5e0df740/9d18784bf836/launch-46ff047ac3db-development.min.js"
 
