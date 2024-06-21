f580046696fc2c8135cad1cb16e9739d4b6d5797
mahesh1245678910 <maheshvangala1997@gmail.com>
2024-06-22 00:25:17 +0530

    commit

--- add_launch_script.js
+++ add_launch_script.js
@@ -14,7 +14,7 @@ dataLayer_script.innerHTML="window.adobeDataLayer = window.adobeDataLayer || [];
 let headTag=document.getElementsByTagName("head")[0];
 
 let launch_script_tag=document.createElement("script");
-launch_script_tag.src= envs["Mahesh Enablement Web Property-SELF"]
+launch_script_tag.src= envs["MV_PRACTICE_APPMEASUREMENT-SELF"]
 launch_script_tag.async=true;
 
 
