433a8bcee3645f67e80596c2fff23b2fd7072d30
mahesh1245678910 <maheshvangala1997@gmail.com>
2024-06-26 18:45:09 +0530

    commit

--- add_launch_script.js
+++ add_launch_script.js
@@ -8,6 +8,35 @@ let envs={
 }
 
 
+// <!-- Google Tag Manager -->
+/* <script>
+//</script> */
+
+// <!-- End Google Tag Manager -->
+
+
+let gscripthead=document.createElement("script");
+gscripthead.innerHTML="(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T6RF2LF');"
+
+
+let gscriptbody=document.createElement("noscript");
+gscriptbody.innerHTML="<iframe src='https://www.googletagmanager.com/ns.html?id=GTM-T6RF2LF' height='0' width='0' style='display:none;visibility:hidden'></iframe>"
+
+
+
+
+
+
+
+
+// <!-- Google Tag Manager (noscript) -->
+// <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T6RF2LF"
+// height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+// <!-- End Google Tag Manager (noscript) -->
+
+
+
+
 let dataLayer_script=document.createElement("script");
 dataLayer_script.id="dataLayer_script";
 dataLayer_script.innerHTML="window.adobeDataLayer = window.adobeDataLayer || [];"
