4d19f9da1a7b737dd4bc07f74da0e420ef78b16c
mahesh1245678910 <maheshvangala1997@gmail.com>
2024-08-11 22:04:14 +0530

    commit

--- add_launch_script.js
+++ add_launch_script.js
@@ -46,7 +46,7 @@ let headTag=document.getElementsByTagName("head")[0];
 
 
 let launch_script_tag=document.createElement("script");
-launch_script_tag.src= envs["MV_PRACTICE_APPMEASUREMENT-SELF"]
+launch_script_tag.src= envs["AA WEB MVANGALA"]
 launch_script_tag.async=true;
 
 
