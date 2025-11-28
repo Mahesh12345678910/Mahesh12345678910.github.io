b7ea115ebf9660497261ea9d9e29c2266aab31c1
mahesh1245678910 <maheshvangala1997@gmail.com>
2025-11-28 09:05:23 +0530

    commit

--- GeometrixxUsers.js
+++ GeometrixxUsers.js
@@ -2,21 +2,21 @@ let GC_Users=[
     {
         "userName" : "mahesh",
         "password" : "mahesh@1234",
-        "userID" : "mvangala"
+        "userID" : "mvangala1"
     },
     {
         "userName" : "ramesh",
         "password" : "ramesh@1234",
-        "userID" : "rthota"
+        "userID" : "rthota1"
     },
     {
         "userName" : "suresh",
         "password" : "suresh@1234",
-        "userID" : "sjakkula"
+        "userID" : "sjakkula1"
     },
     {
         "userName" : "naresh",
         "password" : "naresh@1234",
-        "userID" : "nsithra"
+        "userID" : "nsithra1"
     },
 ]
